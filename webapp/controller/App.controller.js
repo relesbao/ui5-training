20d017d70b704b621b5093b95338ed64c39429a1
Rodrigo da Rosa Elesbão <rodrigo.elesbao@sap.com>
2018-09-27 13:21:27 -0300

    added button to create a new todo

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,6 +6,12 @@ sap.ui.define([
 	return Controller.extend("meta.training.example.controller.App", {
 		onInit: function(){
 			this.getView().setModel(new Todo(), "TodoModel");
+		},
+
+		onAddTodo: function() {
+			var oData = this.getView().getModel("TodoModel").getData();
+			oData.push({todo: "", completed: false});
+			this.getView().getModel("TodoModel").setData(oData);
 		}
 	});
 });
