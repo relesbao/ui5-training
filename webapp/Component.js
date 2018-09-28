e0a9f672fe519d358c8025cb3bd557cae5bc74f4
Rodrigo da Rosa Elesbão <rodrigo.elesbao@sap.com>
2018-09-28 19:56:02 -0300

    Fixed component

--- webapp/Component.js
+++ webapp/Component.js
@@ -3,6 +3,12 @@ sap.ui.define(["sap/ui/core/UIComponent"], function(UIComponent){
 	return UIComponent.extend('meta.training.example.Component', {
 		metadata: {
 			manifest: "json"
+		},
+
+		init: function() {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this.getRouter().initialize();
 		}
 	});
 });
